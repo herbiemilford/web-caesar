168127290a84be63f2383028cb0f202737869020
Joe Crowder <toast.one.art@gmail.com>
2018-10-13 15:40:39 -0500

    create form

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+      <!-- create your form here -->
+      <form>
+      <label for="rotate-by" >Rotate by: <label/>
+      <input id="rotate-by" type="text" name="rot" value="0"/>
+      <input type="submit" />
+      <textarea name="text">enter stuff here</textarea>
+      
+      </form>
+    </body>
+</html>
+"""
+@app.route("/")
+def index():
+    return form
+
+app.run()
